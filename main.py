3f97629c89f2cda7bf4a8c2edaf6ab25bd983922
Max Tanous <mctanous@bowdoin.edu>
2018-09-25 12:26:56 -0400

    changes to main

--- main.py
+++ main.py
@@ -13,24 +13,25 @@ import time
 
 class main:
 
-
+	"This Program is used to to runb our various algorithms to buy and sell"
+	"cryptocurrency (LTC)"
 
 
 	global acceptable_loss,  total_loss, clock, market, coinbase_terminal, setPrice, dollar_buy
 
 	#User Input information for keys
-	User_API_Key = "fb7270674e21167ee5267782f4f5c941"
-	User_Secret= "dauisy8htpppF/mCODwgqPP94CYK9TJ8NiUrnQ62rJ9FRA0td2Szv8WpFHErdrIa7bJPpTK1CDXU7/DY/yjs+Q=="
-	User_Passphrase = "notaphassphrase"
+
 
 	#dollar_buy = float(input("How much do you want to spend?: "))
 	#setPrice = float(input("What is the price you want to buy at? :"))
-
-	coinbase_terminal = CoinbaseExchangeAuth(User_API_Key,User_Secret, User_Passphrase)
+	
+	"Open Terminal to begin purchasin"
+	#coinbase_terminal = CoinbaseExchangeAuth(User_API_Key,User_Secret, User_Passphrase)
 
 
 	#market = MarketSocket.MarketSocket(coinbase_terminal, 'https://api.gdax.com/products/ltc-usd/ticker')
 	#auth_client = AuthenticatedClient(coinbase_terminal, market, api_url)
+	"Run Algorithms from market data"
 	#algo = Algorithm("dummy", auth_client, market, "10.00", "LTC-USD")
 
 
